4e58d4fa9586df71868c300219425d16c15cb73e
Praktikant <praktikant@int.gsits.de>
2020-11-26 16:13:19 +0100

    Added basic post pull

---
+++ helper/db_helper.js
@@ -0,0 +1,48 @@
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    port: '3306',
+    database: 'newsblog'
+});
+
+connection.connect(function (err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+
+
+function getNews(callback) {
+    connection.query('SELECT NewsID, headline, content, date, firstname, surname FROM news, author', (error, result, field) => {
+        if (error) console.log(error)
+        return callback(null, result);
+    })
+}
+
+function getPost(postID, callback) {
+    var query = 'SELECT NewsID, headline, content, date, firstname, surname FROM news, author WHERE news.NewsID=' + postID; 
+    console.log(query)
+    connection.query(query, (error, result) => {
+        if (error) console.log(error)
+        return callback(null, result);
+    })
+}
+
+
+function getHeadline(callback) {
+    connection.query('SELECT headline FROM news', (error, result, field) => {
+        if (error) console.log(error)
+        return callback(result);
+    })
+}
+
+
+exports.getHeadline = getHeadline;
+exports.getNews = getNews
+exports.getPost = getPost
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../helper/db_helper');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
   res.render('index', { title: 'Express' });
+});
+
+/* GET home page. */
+router.get('/tmp', function(req, res, next) {
+  var myres = null
+  db.getNews(function(err, data){
+    var post;
+    for(post of data){
+      console.log(post.content)
+      post.content = post.content.substring(0,500)
+      console.log(post.content)
+    }
+    var out = [data[0], data[0]]
+    res.render('tmplate', { title: 'Express', news: out });
+    console.log(myres)
+  })
+});
+
+/* GET wirte Post. */
+router.get('/write', function(req, res, next) {
+
+  res.render('write', { title: 'Express', news: null });
+});
+
+
+/* GET post page. */
+router.get(/\/post1?[0-9]$/, function(req, res, next) {
+  var post_num = req.url.match(/\d+/)[0];
+  console.log(post_num)
+  db.getPost(post_num, function(err, data){
+    res.render('post', { title: 'Express', news: data[0] });
+  })
 });
 
 module.exports = router;
