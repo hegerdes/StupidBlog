89eb8a330df72314b09d35575e57dadccb5b243e
Praktikant <praktikant@int.gsits.de>
2020-11-27 11:32:36 +0100

    Working state

--- helper/db_helper.js
+++ helper/db_helper.js
@@ -1,55 +1,105 @@
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    port: '3306',
-    database: 'newsblog'
+  host: "127.0.0.1",
+  user: "root",
+  password: "",
+  port: "3306",
+  database: "newsblog",
 });
 
 connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
-
-
 function getNews(callback) {
-    connection.query('SELECT NewsID, headline, content, date, firstname, surname FROM news, author', (error, result, field) => {
-        if (error) console.log(error)
-        return callback(null, result);
-    })
+  connection.query(
+    "SELECT NewsID, headline, content, date, firstname, surname FROM news, author",
+    (error, result, field) => {
+      if (error) console.log(error);
+      return callback(null, result);
+    }
+  );
 }
 
 function getPost(postID, callback) {
-    var query = 'SELECT NewsID, headline, content, date, firstname, surname FROM news, author WHERE news.NewsID=' + postID; 
-    console.log(query)
-    connection.query(query, (error, result) => {
-        if (error) console.log(error)
-        return callback(null, result);
-    })
+  var query =
+    "SELECT NewsID, headline, content, date, firstname, surname FROM news, author WHERE news.NewsID=" +
+    connection.escape(postID);
+  console.log(query);
+  connection.query(query, (error, result) => {
+    if (error) console.log(error);
+    return callback(null, result);
+  });
 }
 
 function getNewsIds(callback) {
-    connection.query('SELECT NewsID FROM news;', (error, result) => {
-        if (error) console.log(error)
-        return callback(result);
-    })
+  connection.query("SELECT NewsID FROM news;", (error, result) => {
+    if (error) callback(error, null);
+    return callback(null, result);
+  });
 }
 
-function getHeadline(callback) {
-    connection.query('SELECT headline FROM news', (error, result, field) => {
-        if (error) console.log(error)
-        return callback(result);
-    })
+function getHeadlines(callback) {
+  connection.query("SELECT headline FROM news", (error, result, field) => {
+    if (error) callback(error, null);
+    return callback(null, result);
+  });
 }
 
+function checkUser(userMail, first, second, callback) {
+  connection.query(
+    "SELECT * FROM author WHERE mail=" + connection.escape(userMail),
+    (error, result, field) => {
+      if (error) callback(error, null);
+      if (result.length > 0) {
+        console.log("Exist");
+        return callback(null, result[0].authorID);
+      } else {
+        console.log("Not exist");
+        var query =
+          "INSERT INTO `author` (`authorID`, `firstname`, `surname`, `salut`, `mail`, `password`) VALUES (NULL, " +
+          connection.escape(first) +
+          ", " +
+          connection.escape(second) +
+          ", '" +
+          "Herr" +
+          "', " +
+          connection.escape(userMail) +
+          ", NULL);";
+        console.log(query);
+        connection.query(query, (error, newresult, field) => {
+          if (error) callback(error, null);
+          return callback(null, newresult.insertId);
+        });
+      }
+      return callback(null, result);
+    }
+  );
+}
 
-exports.getHeadline = getHeadline;
-exports.getNews = getNews
-exports.getNewsIds = getNewsIds
-exports.getPost = getPost
+function postStory(userID, story, headline, callback) {
+  var query =
+    "INSERT INTO `news` (`NewsID`, `authorID`, `headline`, `content`, `date`) VALUES (NULL, " +
+    userID +
+    ", " +
+    connection.escape(headline) +
+    ", " +
+    connection.escape(story) +
+    ", now());";
+  connection.query(query, (error, result, field) => {
+    if (error) callback(error, null);
+    return callback(null, result.insertId);
+  });
+}
+
+exports.postStory = postStory;
+exports.checkUser = checkUser;
+exports.getHeadlines = getHeadlines;
+exports.getNews = getNews;
+exports.getNewsIds = getNewsIds;
+exports.getPost = getPost;
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,84 @@
-var express = require('express');
+var express = require("express");
+const { body, validationResult } = require("express-validator");
 var router = express.Router();
-// var db = require('../helper/db_helper');
+var db = require("../helper/db_helper");
 
+var storryIDs = null;
 
+db.getNewsIds(function (err, data) {
+  if (err) console.log(err);
+  storryIDs = data;
+  console.log(storryIDs);
+});
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   var postIDs
-//   db.getNewsIds( function(err, data){
-//     postIDs = data
-//     console.log(data)
-//   })
-//   res.render('index', { title: 'Express' });
-// });
-
-// /* GET home page. */
-// router.get('/tmp', function(req, res, next) {
-//   var myres = null
-//   db.getNews(function(err, data){
-//     var post;
-//     for(post of data){
-//       console.log(post.content)
-//       post.content = post.content.substring(0,500)
-//       console.log(post.content)
-//     }
-//     var out = [data[0], data[0]]
-//     res.render('tmplate', { title: 'Express', news: out });
-//     console.log(myres)
-//   })
-// });
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  db.getNews(function (err, data) {
+    if (err) {
+      console.log("ERRR");
+      return res.render("error", { message: "Can't get News", status: 500 });
+    } else {
+      var post;
+      for (post of data) {
+        post.content = post.content.substring(0, 500);
+      }
+      console.log(data[0]);
+      return res.render("tmplate", { title: "Express", news: data });
+    }
+  });
+});
 
 /* GET wirte Post. */
-router.get('/write', function(req, res, next) {
-  res.render('write', { title: 'Express' });
+router.get("/write", function (req, res, next) {
+  res.render("write");
 });
 
 /* POST wirte Post. */
-router.post('/write', function(req, res, next) {
-  var userinput = req.body.text;
-  console.log(req.body)
-  res.redirect('/write')
+router.post("/write", function (req, res, next) {
+  // if(req.body.story.length < 5) {
+  //   res.status(400).json({ errors: 'Story too short' });
+  //   return;
+  // }
+  // if(req.body.email.length < 5) {
+  //   res.status(400).json({ errors: 'Mail too short' });
+  //   return;
+  // }
+  // if(req.body.title.length < 5) {
+  //   res.status(400).json({ errors: 'Title too short' });
+  //   return;
+  // }
+  db.checkUser(req.body.email, "New", "User", function (err, data) {
+    if (err) console.log(err);
+    console.log(data);
+    db.postStory(data, req.body.story, req.body.title, function (err, newdata) {
+      if (err) {
+        console.log(err);
+        return res.render("error", { message: err, status: 500 });
+      } else {
+        console.log(newdata);
+        return res.redirect("/post_" + newdata);
+      }
+    });
+  });
 });
 
-
 /* GET post page. */
-router.get(/\/post1?[0-9]$/, function(req, res, next) {
+router.get(/\/post_([0-9]+)$/, function (req, res, next) {
   var post_num = req.url.match(/\d+/)[0];
-  console.log(post_num)
-  // db.getPost(post_num, function(err, data){
-  //   res.render('post', { title: 'Express', news: data[0] });
-  // })
+  console.log(post_num);
+  console.log(storryIDs);
+  var found = false;
+  for (story of storryIDs) {
+    if (story.NewsID == post_num) {
+      found = true;
+      db.getPost(post_num, function (err, data) {
+        res.render("post", { title: "Express", news: data[0] });
+      });
+    }
+  }
+  if (!found) {
+    res.render("error", { message: "Post not found", status: 404 });
+  }
 });
 
 module.exports = router;
